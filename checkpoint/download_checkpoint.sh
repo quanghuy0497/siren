fee1f40626279eb2bce084ee7bf6d076eadbbd69
quanghuy0497 <quanghuy0497@gmail.com>
2024-01-02 10:51:31 +0000

    Update checkpoint download using gdown

---
+++ checkpoint/download_checkpoint.sh
@@ -0,0 +1,4 @@
+for data in "1-9ssnAL4UPv4sOpm8-jfrqgPMIbZ82NV" "1ZUr-ytjtDOYHfeM1geE_MI5B0rY0aisa" "1O_EoEQMSNDMBrAVn0Opr56BY_lSS-1P-" "1QOEMAUj0E9KWNM9e-jmlNw2LjTQv2om8" "1o5yhkgPUhmlp9Co1DCuh_li6kwATUoET"
+do 
+    gdown $data
+done
